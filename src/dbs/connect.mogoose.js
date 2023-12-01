9fa5f45702324e3578674c45e3e5e0567c5af18e
noname21-stack <vanhuybuivips@gmail.com>
2023-12-01 11:56:19 +0700

    update connectString mongooseDB

--- src/dbs/connect.mogoose.js
+++ src/dbs/connect.mogoose.js
@@ -3,9 +3,10 @@
 const mongoose = require('mongoose');
 
 
-const { db: { host, name, port } } = require('../configs/config.mongodb');
+// const { db: { host, name, port } } = require('../configs/config.mongodb');
 
-const connectString = `mongodb://${host}:${port}/${name}`;
+// const connectString = `mongodb://${host}:${port}/${name}`;
+const connectString = `mongodb+srv://vanhuybuivips:SK3ABmPU8tZ8rrhq@cluster0.qj73u8a.mongodb.net/`
 console.log(connectString, "connect String");
 class Database {
     constructor() {
@@ -18,7 +19,7 @@ class Database {
         }
         mongoose.connect(connectString,
             { maxPoolSize: 50 })
-            .then(_ => { console.log("Connect Mongoose Success Vipp")})
+            .then(_ => { console.log("Connect Mongoose Success Vipp") })
             .catch(err => { console.log("Error Connect!"); })
     }
     static getInstance() {
